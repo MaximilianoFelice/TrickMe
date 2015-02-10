0d8ea88c14c1a46b8ffaa78d173dd81af2672688
MaximilianoFelice <maximilianofelice@gmail.com>
2015-02-10 03:15:55 -0300

    Fixed test bug: Asserting asynchronous results

--- src/test/scala/SystemInit.scala
+++ src/test/scala/SystemInit.scala
@@ -4,6 +4,7 @@ import TrickMe._
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{TestProbe, TestKit}
 import org.scalatest.FunSuiteLike
+import org.scalatest.concurrent.AsyncAssertions.Waiter
 
 import scala.util.{Success, Try}
 
@@ -24,13 +25,17 @@ class SystemInit extends TestKit(ActorSystem("InitTest")) with FunSuiteLike {
     val sys = newSystem
     val main = TestProbe()
 
+    val waiter = new Waiter
+
     var elems = List[Try[InitialResult]]()
 
     def doAssertion = assert(elems.toSet == Set(Success("elem1", List()), Success("elem 2", List())))
-    
-    initStream.subscribe({elem => elems = elem :: elems}, {err => throw err},  {() => doAssertion})
+
+    initStream.subscribe({elem => elems = elem :: elems}, {err => throw err},  {() => waiter{doAssertion}; waiter.dismiss})
 
     main.send(sys, Start(Set("elem1", "elem 2")))
+
+    waiter.await()
 
   }
 
